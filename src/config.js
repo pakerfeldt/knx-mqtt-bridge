f7e2ef502fe2d4f311c5c29b58d441092a045a49
Simon Christmann <simon@christmann.email>
2021-08-16 13:32:54 +0200

    wrong whitespace character; access object safely
    Signed-off-by: Simon Christmann <simon@christmann.email>

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ const yaml = require('js-yaml');
 const fs   = require('fs');
 
 exports.parse = function () {
-    const file = process.env.KNX_MQTT_CONFIG || 'config.yaml';
+    const file = process.env.KNX_MQTT_CONFIG || 'config.yaml';
     if (fs.existsSync(file)) {
         try {
           let config =  yaml.safeLoad(fs.readFileSync(file, 'utf8'));
           
-          if (config.mqtt.options.key) config.mqtt.options.key = fs.readFileSync( config.mqtt.options.key );
-          if (config.mqtt.options.cert) config.mqtt.options.cert = fs.readFileSync( config.mqtt.options.cert );
-          if (config.mqtt.options.ca) config.mqtt.options.ca = fs.readFileSync( config.mqtt.options.ca );
+          if (config?.mqtt?.options?.key) config.mqtt.options.key = fs.readFileSync( config.mqtt.options.key );
+          if (config?.mqtt?.options?.cert) config.mqtt.options.cert = fs.readFileSync( config.mqtt.options.cert );
+          if (config?.mqtt?.options?.ca) config.mqtt.options.ca = fs.readFileSync( config.mqtt.options.ca );
           
           return config
         } catch (e) {
